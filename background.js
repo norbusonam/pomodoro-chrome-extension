5e4d3b677c09c392db93b063ecbbe1c9103b9f07
norbusonam <nsonam1822@gmail.com>
2022-07-04 17:06:36 -0400

    reverted to old commit and made popup a full window

--- background.js
+++ background.js
@@ -1,11 +1,5 @@
-const POMODORO = 'pomodoro'
-const SHORTBREAK = 'short-break'
-const LONGBREAK = 'long-break'
-const INPROG = 'inprog'
-const STOPPED = 'stopped'
-const NOTSTARTED = 'not-started'
-
-let timerId = null
+const HEIGHT = 330
+const WIDTH = 400
 
 // initial set up
 chrome.runtime.onInstalled.addListener(() => {
@@ -14,108 +8,14 @@ chrome.runtime.onInstalled.addListener(() => {
         shortBreakLength: 5 * 60,
         longBreakLength: 15 * 60,
     })
-    chrome.storage.local.set({
-        currentTimer: POMODORO,
-        timerState: NOTSTARTED,
-        timeElapsed: 0,
-    })
 })
 
-// notification function
-const sendNotification = (currentTimer) => {
-    let title
-    let message
-    if (currentTimer === POMODORO) {
-        title = 'Pomodoro complete! 🚀'
-        message = 'Ready for a short break?'
-    } else if (currentTimer === SHORTBREAK) {
-        title = 'Short break finished! ☕️'
-        message = 'Ready for a pomodoro?'
-    } else if (currentTimer === LONGBREAK) {
-        title = 'Long break finsihed! ☕️'
-        message = 'Ready for a pomodoro?'
-    }
-    chrome.notifications.clear('timer')
-    chrome.notifications.create('timer', {
-        title,
-        message,
-        buttons: [
-            {
-                title: 'Yes'
-            },
-            {
-                title: 'Not now'
-            },
-        ],
-        type: 'basic',
-        priority: 2,
-        iconUrl: './bell.png',
-        requireInteraction: true,
+// extention icon clicked
+chrome.action.onClicked.addListener(() => {
+    chrome.windows.create({
+        width: WIDTH,
+        height: HEIGHT,
+        url: './index.html',
+        type: 'popup',
     })
-}
-
-// on notification button clicked
-chrome.notifications.onButtonClicked.addListener((notificationId, buttonIndex) => {
-    if (notificationId === 'timer' && buttonIndex == 0) {
-        chrome.storage.sync.get(['pomodoroLength', 'shortBreakLength', 'longBreakLength'], result => {
-            chrome.storage.local.get(['currentTimer'], r => {
-                let length
-                if (r.currentTimer === POMODORO) length = result.pomodoroLength
-                else if (r.currentTimer === SHORTBREAK) length = result.shortBreakLength
-                else if (r.currentTimer === LONGBREAK) length = result.longBreakLength
-                startTimer(length)
-            })
-        })
-    }
-})
-
-// start timer
-const startTimer = (length) => {
-    chrome.storage.local.set({
-        timerState: INPROG,
-    })
-    if (!!timerId) return
-    timerId = setInterval(() => {
-        chrome.storage.local.get(['timeElapsed'], result => {
-            chrome.storage.local.set({
-                timeElapsed: result.timeElapsed + 1
-            })
-            if (result.timeElapsed + 1 >= length) {
-                clearInterval(timerId)
-                timerId = null
-                chrome.storage.local.set({ 
-                    timeElapsed: 0,
-                    timerState: NOTSTARTED,
-                })
-                chrome.storage.local.get(['currentTimer'], r => {
-                    if (r.currentTimer === POMODORO) {
-                        chrome.storage.local.set({ currentTimer: SHORTBREAK })
-                    } else if (r.currentTimer === SHORTBREAK || r.currentTimer === LONGBREAK) {
-                        chrome.storage.local.set({ currentTimer: POMODORO })
-                    }
-                    sendNotification(r.currentTimer)
-                })
-            }
-        })
-    }, 1000)
-}
-
-// receive messages
-chrome.runtime.onMessage.addListener(message => {
-
-    chrome.notifications.clear('timer')
-
-    if (message.type === 'START') {
-        startTimer(message.length)
-    } else if (message.type === 'STOP') {
-        clearInterval(timerId)
-        timerId = null
-        chrome.storage.local.set({ timerState: STOPPED })
-    } else if (message.type === 'RESET') {
-        chrome.storage.local.set({ 
-            timerState: NOTSTARTED,
-            timeElapsed: 0,
-        })
-    }
-
 })
--- index.js
+++ index.js
@@ -2,17 +2,45 @@
 const POMODORO = 'pomodoro'
 const SHORTBREAK = 'short-break'
 const LONGBREAK = 'long-break'
-const INPROG = 'inprog'
-const STOPPED = 'stopped'
-const NOTSTARTED = 'not-started'
+const NOTIFICATIONID = 'timer-notify'
+const HEIGHT = 330
+const WIDTH = 400
 
 // state
 let pomodoroLength = 25 * 60
 let shortBreakLength = 5 * 60
 let longBreakLength = 15 * 60
-let currentTimer = POMODORO
-let timerState = NOTSTARTED
+let currentTimer = POMODORO 
+let activeTimer = null
 let timeElapsed = 0
+let timerId = null
+
+// get latest length values
+chrome.storage.sync.get(['pomodoroLength', 'shortBreakLength', 'longBreakLength'],
+    (result) => {
+        pomodoroLength = !!result.pomodoroLength ? result.pomodoroLength : pomodoroLength
+        shortBreakLength = !!result.shortBreakLength ? result.shortBreakLength : shortBreakLength
+        longBreakLength = !!result.longBreakLength ? result.longBreakLength : longBreakLength
+        updateCountdown()
+    }
+)
+
+// update timer lengths
+const updatePomodoroLength = (length) => {
+    pomodoroLength = length
+    chrome.storage.sync.set({ pomodoroLength: length })
+    updateCountdown()
+}
+const updateShortBreakLength = (length) => {
+    shortBreakLength = length
+    chrome.storage.sync.set({ shortBreakLength: length })
+    updateCountdown()
+}
+const updateLongBreakLength = (length) => {
+    longBreakLength = length
+    chrome.storage.sync.set({ longBreakLength: length })
+    updateCountdown()
+}
 
 // elements
 const tabs = document.querySelectorAll('.tabs li')
@@ -22,62 +50,8 @@ const shorterButton = document.querySelector('#shorterButton')
 const stopResetButton = document.querySelector('#stopResetButton')
 const startButton = document.querySelector('#startButton')
 
-// load length values
-chrome.storage.sync.get(['pomodoroLength', 'shortBreakLength', 'longBreakLength'],
-    result => {
-        pomodoroLength = !!result.pomodoroLength ? result.pomodoroLength : pomodoroLength
-        shortBreakLength = !!result.shortBreakLength ? result.shortBreakLength : shortBreakLength
-        longBreakLength = !!result.longBreakLength ? result.longBreakLength : longBreakLength
-        makePage()
-    }
-)
-
-// load timer state
-chrome.storage.local.get(['currentTimer', 'timerState', 'timeElapsed'],
-    result => {
-        currentTimer = !!result.currentTimer ? result.currentTimer : currentTimer
-        timerState = !!result.timerState ? result.timerState : timerState
-        timeElapsed = !!result.timeElapsed ? result.timeElapsed : timeElapsed
-        makePage()
-    }
-)
-
-// subscribe to changes
-chrome.storage.onChanged.addListener(changes => {
-    timeElapsed = !!changes.timeElapsed ? changes.timeElapsed.newValue : timeElapsed
-    timerState = !!changes.timerState ? changes.timerState.newValue : timerState
-    currentTimer = !!changes.currentTimer ? changes.currentTimer.newValue : currentTimer
-    pomodoroLength  = !!changes.pomodoroLength ? changes.pomodoroLength.newValue : pomodoroLength
-    shortBreakLength = !!changes.shortBreakLength ? changes.shortBreakLength.newValue : shortBreakLength
-    longBreakLength = !!changes.longBreakLength ? changes.longBreakLength.newValue : longBreakLength
-    makePage()
-})
-
-// make page
-const makePage = () => {
-
-    // update buttons
-    if (timerState === NOTSTARTED) {
-        shorterButton.disabled = false
-        longerButton.disabled = false
-        stopResetButton.disabled = true
-        stopResetButton.innerHTML = 'Stop'
-        startButton.disabled = false
-    } else if (timerState === STOPPED) {
-        shorterButton.disabled = true
-        longerButton.disabled = true
-        stopResetButton.disabled = false
-        stopResetButton.innerHTML = 'Reset'
-        startButton.disabled = false
-    } else if (timerState === INPROG) {
-        shorterButton.disabled = true
-        longerButton.disabled = true
-        stopResetButton.disabled = false
-        stopResetButton.innerHTML = 'Stop'
-        startButton.disabled = true
-    }
-    
-    // update countdown
+// initialize countdown
+const updateCountdown = () => {
     let timeLeft
     if (currentTimer === POMODORO) {
         timeLeft = pomodoroLength - timeElapsed
@@ -89,89 +63,165 @@ const makePage = () => {
     const minutesLeft = `${Math.floor(timeLeft / 60)}`.padStart(2, '0')
     const secondsLeft = `${timeLeft % 60}`.padStart(2, '0')
     countdown.innerHTML = `${minutesLeft}:${secondsLeft}`
+}
 
-    // update tabs
+// switching timers
+const switchTimer = (timer) => {
+    currentTimer = timer
+    timeElapsed = 0
+    activeTimer = null
+    startButton.disabled = false
+    stopResetButton.disabled = true
+    longerButton.disabled = false
+    shorterButton.disabled = false
+    chrome.notifications.clear(NOTIFICATIONID)
+    clearInterval(timerId)
+    updateCountdown()
     tabs.forEach(tab => {
+        tab.classList.remove('is-disabled')
         if (tab.dataset.target === currentTimer) {
             tab.classList.add('is-active')
         } else {
             tab.classList.remove('is-active')
         }
     })
-    if (timerState === NOTSTARTED) {
-        tabs.forEach(tab => tab.classList.remove('is-disabled'))
-    } else {
-        tabs.forEach(tab => {
-            if (tab.dataset.target === currentTimer) {
-                tab.classList.remove('is-disabled')
-            } else {
-                tab.classList.add('is-disabled')
-            }
-        })
-    }
-
 }
-
-// on switch tab
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         selectedTimer = tab.dataset.target
         if (selectedTimer !== currentTimer) {
-            chrome.notifications.clear('timer')
-            currentTimer = selectedTimer
-            chrome.storage.local.set({ currentTimer })
+            switchTimer(selectedTimer)
         }
     })
 })
 
-// on longer pressed
+// notification function
+const sendNotification = (timerFinished) => {
+    let title
+    let message
+    if (timerFinished === POMODORO) {
+        title = 'Pomodoro complete! 🚀'
+        message = 'Ready for a short break?'
+    } else if (timerFinished === SHORTBREAK) {
+        title = 'Short break finished! ☕️'
+        message = 'Ready for a pomodoro?'
+    } else if (timerFinished === LONGBREAK) {
+        title = 'Long break finsihed! ☕️'
+        message = 'Ready for a pomodoro?'
+    }
+    chrome.notifications.clear(NOTIFICATIONID)
+    new Audio('./sounds/alarm.wav').play()
+    chrome.notifications.create(NOTIFICATIONID, {
+        title,
+        message,
+        buttons: [
+            {
+                title: 'Yes'
+            },
+            {
+                title: 'Not now'
+            },
+        ],
+        type: 'basic',
+        priority: 2,
+        iconUrl: './icons/icon128.png',
+        requireInteraction: true,
+    })
+}
+
+// longer pressed
 longerButton.addEventListener('click', () => {
     if (currentTimer === POMODORO) {
-        pomodoroLength += 60
-        chrome.storage.sync.set({ pomodoroLength })
+        updatePomodoroLength(pomodoroLength + 60)
     } else if (currentTimer === SHORTBREAK) {
-        shortBreakLength += 60
-        chrome.storage.sync.set({ shortBreakLength })
+        updateShortBreakLength(shortBreakLength + 60)
     } else if (currentTimer === LONGBREAK) {
-        longBreakLength += 60
-        chrome.storage.sync.set({ longBreakLength })
+        updateLongBreakLength(longBreakLength + 60)
     }
 })
 
-// on shorter pressed
+// shorter pressed
 shorterButton.addEventListener('click', () => {
     if (currentTimer === POMODORO) {
-        pomodoroLength = Math.max(60, pomodoroLength - 60)
-        chrome.storage.sync.set({ pomodoroLength })
+        updatePomodoroLength(Math.max(60, pomodoroLength - 60))
     } else if (currentTimer === SHORTBREAK) {
-        shortBreakLength = Math.max(60, shortBreakLength - 60)
-        chrome.storage.sync.set({ shortBreakLength })
+        updateShortBreakLength(Math.max(60, shortBreakLength - 60))
     } else if (currentTimer === LONGBREAK) {
-        longBreakLength = Math.max(60, longBreakLength - 60)
-        chrome.storage.sync.set({ longBreakLength })
+        updateLongBreakLength(Math.max(60, longBreakLength - 60))
     }
 })
 
 // start pressed
 startButton.addEventListener('click', () => {
-    let length
-    if (currentTimer === POMODORO) length = pomodoroLength
-    else if (currentTimer === SHORTBREAK) length = shortBreakLength
-    else if (currentTimer === LONGBREAK) length = longBreakLength
-    chrome.runtime.sendMessage({ 
-        type: 'START',
-        length,
+    longerButton.disabled = true
+    shorterButton.disabled = true
+    startButton.disabled = true
+    stopResetButton.disabled = false
+    activeTimer = currentTimer
+    stopResetButton.innerHTML = 'Stop'
+    chrome.notifications.clear(NOTIFICATIONID)
+    timerId = setInterval(() => {
+        timeElapsed += 1
+        updateCountdown()
+        if (currentTimer === POMODORO && timeElapsed === pomodoroLength) {
+            timerFinished = currentTimer
+            switchTimer(SHORTBREAK)
+            sendNotification(timerFinished)
+        } else if (currentTimer === SHORTBREAK && timeElapsed === shortBreakLength) {
+            timerFinished = currentTimer
+            switchTimer(POMODORO)
+            sendNotification(timerFinished)
+        } else if (currentTimer === LONGBREAK && timeElapsed === longBreakLength) {
+            timerFinished = currentTimer
+            switchTimer(POMODORO)
+            sendNotification(timerFinished)
+        }
+    }, 1000)
+    tabs.forEach(tab => {
+        if (tab.dataset.target === currentTimer) {
+            tab.classList.remove('is-disabled')
+        } else {
+            tab.classList.add('is-disabled')
+        }
     })
 })
 
 // stop/reset pressed
 stopResetButton.addEventListener('click', () => {
+    clearInterval(timerId)
+    startButton.disabled = false
     if (stopResetButton.innerHTML === 'Stop') {
-        chrome.runtime.sendMessage({ type: 'STOP' })
+        stopResetButton.innerHTML = 'Reset'
     } else if (stopResetButton.innerHTML === 'Reset') {
-        chrome.runtime.sendMessage({ type: 'RESET' })
+        timeElapsed = 0
+        updateCountdown()
+        stopResetButton.innerHTML = 'Stop'
+        stopResetButton.disabled = true
+        longerButton.disabled = false
+        shorterButton.disabled = false
+        tabs.forEach(tab => {
+            tab.classList.remove('is-disabled')
+        })
     }
 })
 
-// clear notifications for opening pop up
-chrome.notifications.clear('timer')
+// on notification button clicked
+chrome.notifications.onButtonClicked.addListener((notificationId, buttonIndex) => {
+    if (notificationId === NOTIFICATIONID && buttonIndex == 0) {
+        startButton.click()
+    }
+})
+
+// fix window size
+window.addEventListener('resize', () => {
+    window.resizeTo(WIDTH, HEIGHT);  
+})
+
+// before closing window
+window.onbeforeunload = () => {
+    if (timeElapsed > 0) {
+        return "Closing this window will cancel the timer in progress. Are you sure you want to proceed?"
+    } else {
+        return
+    }
+};
